17da26929d85be0a0975a6835589a4ebaa0cd0d8
keshavg <keshavagowtham@gmail.com>
2017-08-08 14:11:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,18 @@
 console.log('Loaded!');
+
+
+var img = document.getElementById('madi');
+
+var marginLeft = 0;
+
+function moveElement()
+{
+   marginLeft = marginLeft + 10;
+   img.style.marginLeft = marginLeft + 'px';
+    
+}
+
+img.onClick = function()
+{
+    var interval = setTimeInterval(moveElement,100);
+};
